58cda2a9a1d9aa126e7a9ae82e87f7a036d48d18
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-11-27 11:49:49 -0300

    cards products

--- TheShop/Scenes/Home/HeaderCollectionReusableView.swift
+++ TheShop/Scenes/Home/HeaderCollectionReusableView.swift
@@ -4,20 +4,32 @@ class HeaderCollectionReusableView: UICollectionReusableView {
 	
 	static let identifier: String = "HeaderCollectionReusableView"
 	
-	lazy var titleLabel: UILabel = {
+	private let titleLabel: UILabel = {
 		$0.translatesAutoresizingMaskIntoConstraints = false
 		$0.text = String(localizedKey: "titleHome")
 		$0.font = UIFont(name: "Bungee-Regular", size: 40)
 		return $0
 	}(UILabel())
 	
+	private let lineView: UIView = {
+		$0.translatesAutoresizingMaskIntoConstraints = false
+		$0.layer.borderWidth = 2
+		$0.layer.borderColor = UIColor.green.cgColor
+		return $0
+	}(UIView())
+	
 	public func configure() {
 		addSubview(titleLabel)
-	}
+		addSubview(lineView)
+		
+		NSLayoutConstraint.activate([
+			titleLabel.topAnchor.constraint(equalTo: topAnchor),
+			lineView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor)
+		])
+}
 	
 	override func layoutSubviews() {
 		super.layoutSubviews()
-		titleLabel.frame = bounds
+		lineView.frame = CGRect(origin: titleLabel.anchorPoint, size: CGSize(width: 100, height: 2))
 	}
-	
 }
--- TheShop/Scenes/Home/HomeView.swift
+++ TheShop/Scenes/Home/HomeView.swift
@@ -1,21 +1,7 @@
 import UIKit
 
 class HomeView: UIView {
-	
-	lazy var titleLabel: UILabel = {
-		$0.translatesAutoresizingMaskIntoConstraints = false
-		$0.text = String(localizedKey: "titleHome")
-		$0.font = UIFont(name: "Bungee-Regular", size: 40)
-		return $0
-	}(UILabel())
-	
-	lazy var lineView: UIView = {
-		$0.translatesAutoresizingMaskIntoConstraints = false
-		$0.layer.borderWidth = 2
-		$0.layer.borderColor = UIColor.green.cgColor
-		return $0
-	}(UIView())
-	
+		
 	lazy var productCollectionView: UICollectionView = {
 		let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout.init()
 		layout.scrollDirection = .vertical
@@ -51,14 +37,6 @@ class HomeView: UIView {
 	
 	private func configConstraints() {
 		NSLayoutConstraint.activate([
-//			titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
-//			titleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 12),
-//			titleLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -12),
-//			
-//			lineView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
-//			lineView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-//			lineView.widthAnchor.constraint(equalToConstant: 120),
-//			lineView.heightAnchor.constraint(equalToConstant: 2),
 			
 			productCollectionView.topAnchor.constraint(equalTo: topAnchor),
 			productCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
